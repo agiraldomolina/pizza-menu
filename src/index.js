38194c0d98ea0ada7f6977d13dc316b713132e40
Alba Giraldo <agiraldomolina@gmail.com>
2024-08-13 01:35:15 -0400

    Creating and reusing component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
 function App(){
-   return <h1>Hello, React!!!!!</h1>;
+   return <div>
+     <h1>My React App</h1>
+     <Pizza/>
+     <Pizza/>
+     <Pizza/>
+   </div> 
+}
+
+function Pizza(){
+  return <div>
+  <img src='pizzas/margherita.jpg' alt='pizza-margherite'/>
+      <h1>Pizza Margherita</h1>
+      <p>Tomato and mozarella</p>
+    </div>
 }
 
 // render the app in the DOM
