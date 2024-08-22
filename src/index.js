334e4c7efffe186e708f6b16216b3cd5f1883adc
Alba Giraldo <agiraldomolina@gmail.com>
2024-08-22 06:42:52 -0400

    rendering Lists

--- src/index.js
+++ src/index.js
@@ -72,32 +72,29 @@ function Menu() {
   return(
     <main className='menu'>
       <h2>Our Menu</h2>
-      <Pizza 
-        name = "Pizza Spinaci" 
-        ingredients="Tomato, mozarella, spinach and ricotta cheese" photoName="pizzas/spinaci.jpg" 
-        price={10}
-      />
-      <Pizza 
-        name = "Pizza Funghi" 
-        ingredients="Tomato, mushrooms and ricotta cheese" 
-        photoName="pizzas/funghi.jpg" 
-        price={15}
-      />
+
+      <ul className='pizzas'>
+        {
+          pizzaData.map((pizza) => 
+            (<Pizza pizzaObj={pizza} key={pizza.name}/>
+            )
+          )
+        }
+      </ul>
     </main>
   )
 }
 
 function Pizza(props){
-  console.log(props)
   return (
-    <div className='pizza'>
-      <img src={props.photoName} alt={props.name}/>
+    <li className='pizza'>
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}/>
       <div>
-        <h3>{props.name} </h3>
-        <p>{props.ingredients} </p>
-        <span>{props.price+3} </span>
+        <h3>{props.pizzaObj.name} </h3>
+        <p>{props.pizzaObj.ingredients} </p>
+        <span>{props.pizzaObj.price+3} </span>
       </div>
-    </div>
+    </li>
   );
 }
 
@@ -107,18 +104,13 @@ function Footer(){
   const closeHour = 20;
   const isOpen = hour >=openHour && hour <= closeHour;
   console.log(isOpen)
-  
-  // if (hour >= openHour && hour <= closeHour)
-  //   alert("We're curremtly open!")
-  // else
-  //   alert("Sorry we're closed")
-
-  return <footer className='footer'>{new Date().toLocaleDateString()} We're currently closed</footer>
-  return 
-  (<footer>{new Date().toLocaleDateString()} We're currently open</footer>)
-  
+  if (isOpen){
+    return (<footer className='footer'>{new Date().toLocaleDateString()} We're currently open</footer>)
+  }
+  else {
+    return (<footer className='footer'>{new Date().toLocaleDateString()} We're currently closed</footer>)
+  }
 }
-
 
 // render the app in the DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
