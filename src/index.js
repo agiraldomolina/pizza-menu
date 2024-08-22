9ad02ffbc6805d2536ae409301b58c67a62ddd59
Alba Giraldo <agiraldomolina@gmail.com>
2024-08-22 13:25:44 -0400

    Conditional rendering with ternaries

--- src/index.js
+++ src/index.js
@@ -70,21 +70,22 @@ function Header(){
 
 function Menu() {
   const pizzas = pizzaData;
+  // const pizzas = [];
   const numPizzas = pizzas.length;
 
   return(
     <main className='menu'>
       <h2>Our Menu</h2>
       {
-        numPizzas > 0 && (
+        numPizzas > 0 ? (
           <ul className='pizzas'>
           {
             pizzas.map((pizza) => 
               (<Pizza pizzaObj={pizza} key={pizza.name}/>)
             )
           }
-        </ul>
-        )
+          </ul>
+        ) : <p>We're still working on our menu. Please come back later :)</p>
       }   
     </main>
   )
@@ -105,21 +106,21 @@ function Pizza(props){
 
 function Footer(){
   const hour = new Date().getHours();
-  const openHour = 4;
+  const openHour = 9;
   const closeHour = 20;
   const isOpen = hour >=openHour && hour <= closeHour;
   console.log(isOpen)
  
   return(
     <footer className='footer'>
-    {isOpen && (
+    {isOpen ? (
       <div className='order'>
         <p>
           We're open until {closeHour}:00. Come visit us or order online
         </p>
         <button className='btn'>Order</button>
       </div>
-    )} 
+): <p>We're closed right now. We're happy to welcome you between {openHour}:00 and {closeHour}:00 </p>} 
     </footer>
   )
 }
