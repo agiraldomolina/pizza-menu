86021cc32b474b8fdd22206a8b4a0ad38e52cc0e
Alba Giraldo <agiraldomolina@gmail.com>
2024-08-22 03:55:24 -0400

    Styling react applications

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./index.css"
 
 const pizzaData = [
   {
@@ -49,7 +50,7 @@ const pizzaData = [
 
 function App(){
    return (
-    <div>
+    <div className="container">
       <Header/>
       <Menu/>
       <Footer/>
@@ -58,17 +59,23 @@ function App(){
 }
 
 function Header(){
-  return <h1>Fast React Pizza Co.</h1>
+  return (
+    <header className='header'>
+      <h1>
+        Fast React Pizza Co.
+      </h1>   
+    </header>
+  );
 }
 
 function Menu() {
   return(
-    <div>
+    <main className='menu'>
       <h2>Our Menu</h2>
       <Pizza/>
       <Pizza/>
       <Pizza/>
-    </div>
+    </main>
   )
 }
 
@@ -84,8 +91,9 @@ function Footer(){
   // else
   //   alert("Sorry we're closed")
 
-  return <footer>{new Date().toLocaleDateString()} We're currently closed</footer>
-  return <footer>{new Date().toLocaleDateString()} We're currently open</footer>
+  return <footer className='footer'>{new Date().toLocaleDateString()} We're currently closed</footer>
+  return 
+  (<footer>{new Date().toLocaleDateString()} We're currently open</footer>)
   
 }
 
@@ -94,7 +102,7 @@ function Footer(){
 function Pizza(){
   return <div>
   <img src='pizzas/margherita.jpg' alt='pizza-margherite'/>
-      <h1>Pizza Margherita</h1>
+      <h3>Pizza Margherita</h3>
       <p>Tomato and mozarella</p>
     </div>
 }
